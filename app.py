3ad3ce876fe1a93d78c235c9caff7c9489a4caf7
ikjai-na <159454911+ikjai-na@users.noreply.github.com>
2024-08-06 01:27:42 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -127,41 +127,41 @@ def main():
                 
                 filename = st.text_input("저장할 파일 이름을 입력하세요 (확장자 제외):", key="filename_hashtags")
                 
-                if len(filename) > 0:
+                # if len(filename) > 0:
                 
                     # if st.button("저장하기", key="save_hashtags"):
-                        content = ""
-                        for data in image_data_list:
-                            content += f"{os.path.basename(data['image_path'])}({data['image_path']})\n"
-                            content += f"이미지에 대한 캡션: {data['caption']}\n\n"
-                        content += f"글: {story}\n\n"
-                        content += f"해시태그: {hashtags}\n"
-                        # save_to_file(content, filename)
-                        st.download_button(
-                            label="파일 다운로드",
-                            data = content,
-                            file_name = filename  + '.txt',
-                            mime = "text/plain"
-                        )
+                content = ""
+                for data in image_data_list:
+                    content += f"{os.path.basename(data['image_path'])}({data['image_path']})\n"
+                    content += f"이미지에 대한 캡션: {data['caption']}\n\n"
+                content += f"글: {story}\n\n"
+                content += f"해시태그: {hashtags}\n"
+                # save_to_file(content, filename)
+                st.download_button(
+                    label="파일 다운로드",
+                    data = content,
+                    file_name = filename  + '.txt',
+                    mime = "text/plain"
+                )
             else:
                 # 8-1) 글까지만 저장
                 filename = st.text_input("저장할 파일 이름을 입력하세요 (확장자 제외):", key="filename_story")
                 
-                if len(filename) > 0:
+                # if len(filename) > 0:
                 
                     # if st.button("저장하기", key="save_story"):
-                        content = ""
-                        for data in image_data_list:
-                            content += f"{os.path.basename(data['image_path'])}({data['image_path']})\n"
-                            content += f"이미지에 대한 캡션: {data['caption']}\n\n"
-                        content += f"글: {story}\n"
-                        # save_to_file(content, filename)
-                        st.download_button(
-                            label="파일 다운로드",
-                            data = content,
-                            file_name = filename  + '.txt',
-                            mime = "text/plain"
-                        )
+                content = ""
+                for data in image_data_list:
+                    content += f"{os.path.basename(data['image_path'])}({data['image_path']})\n"
+                    content += f"이미지에 대한 캡션: {data['caption']}\n\n"
+                content += f"글: {story}\n"
+                # save_to_file(content, filename)
+                st.download_button(
+                    label="파일 다운로드",
+                    data = content,
+                    file_name = filename  + '.txt',
+                    mime = "text/plain"
+                )
         else:
             st.warning("캡션만 저장, 글 생성 중 선택하시기 바랍니다.")
 
