09af6b1f048d2243a58e850316e7769d631f4c0f
ikjai-na <159454911+ikjai-na@users.noreply.github.com>
2024-08-06 00:50:04 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -104,6 +104,9 @@ def main():
             generate_hashtags = st.checkbox("해시태그를 생성하시겠습니까?")
             generate_writing = st.button("글 생성")
             
+            st.write(f"generate_hashtags => {generate_hashtags}")
+            st.write(f"generate_writing => {generate_writing}")
+            
             if generate_writing:
                 # 6) 글 생성
                 story = writer.write_story(image_data_list, user_context, writing_style, writing_length, temperature, user_info)
